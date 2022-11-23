3826442923ed7bfcf177c3614a3d5e12b5a723ae
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2022-11-23 17:33:34 +0100

    Fixed gcc issue.

--- src/Memory.hpp
+++ src/Memory.hpp
@@ -61,15 +61,6 @@ namespace Tools
             return ptr;
         }
     }
-
-    force_inline void aligned_free(void * ptr)
-    {
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-        _aligned_free( ptr );
-#else
-        free( ptr );
-#endif
-    }
     
     template <typename T>
     force_inline int safe_free( T * & ptr )
