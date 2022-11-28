e9134683506f5fbcd8d118d340995dc728730a73
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2022-11-28 09:26:33 +0100

    Restructuring.

---
+++ src/ToString.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace Tools
+{
+    template <typename T>
+    std::string ToString(const T & a_value, const int p = 16)
+    {
+        std::stringstream sout;
+        sout.precision(p);
+        sout << a_value;
+        return sout.str();
+    }
+
+    template <typename T>
+    std::string ToString(const std::complex<T> & z, const int p = 16)
+    {
+        std::stringstream sout;
+        sout.precision(p);
+        sout << real(z) << ((imag(z) < static_cast<T>(0)) ? " + " : " - ") << std::abs(imag(z)) << "I" ;
+        return sout.str();
+    }
+    
+    template<typename T>
+    std::string ToString( const std::vector<T> & a, const int p = 16)
+    {
+        std::stringstream sout;
+        sout.precision(p);
+        sout << "{ ";
+        if( a.size() > 0 )
+        {
+            sout << a[0];
+        }
+        
+        for( size_t i = 1; i < a.size(); ++i )
+        {
+            sout << ", " << ToString(a[i]);
+        }
+        sout << " }";
+        return sout.str();
+    }
+
+} // namespace Tools
