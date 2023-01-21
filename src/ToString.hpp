fcebf000724be41ced6a78a5dc51956fd88ede1b
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2023-01-21 14:44:01 +0100

    Arrays to string.

--- src/ToString.hpp
+++ src/ToString.hpp
@@ -21,41 +21,133 @@ namespace Tools
     }
     
     template<typename T>
-    std::string ToString( const std::vector<T> & a, const int p = 16)
+    struct Array1DToString
     {
-        std::stringstream sout;
-        sout.precision(p);
-        sout << "{ ";
-        if( a.size() > 0 )
+        Array1DToString() = default;
+        ~Array1DToString() = default;
+        
+        std::string header = "";
+        
+        std::string vector_prefix = "{ ";
+        
+            std::string sep = ", ";
+        
+        std::string vector_suffix = "\n}";
+        
+        std::string footer = "";
+        
+        int prec = 16;
+        
+        std::string operator()( const * restrict const a, const size_t n ) const
         {
-            sout << a[0];
+            std::stringstream sout;
+         
+            sout.precision(prec);
+            
+            sout << header;
+            
+            sout << vector_prefix;
+            {
+                if( n > 0 )
+                {
+                    sout << a[0];
+                }
+                
+                for( size_t i = 1; i < n; ++i )
+                {
+                    sout << sep << a[i];
+                }
+            }
+            sout << vector_suffix;
+            
+            sout << footer;
+            
+            return sout.str();
         }
         
-        for( size_t i = 1; i < a.size(); ++i )
-        {
-            sout << ", " << a[i];
-        }
-        sout << " }";
-        return sout.str();
-    }
+    } // Array1DToString
     
     template<typename T>
-    std::string ToString( const T * const a, const size_t n, const int p = 16)
+    struct Array2DToString
     {
-        std::stringstream sout;
-        sout.precision(p);
-        sout << "{ ";
-        if( n > 0 )
+        Array2DToString() = default;
+        
+        explicit Array2DToString( int prec )
+        :   row_converter( prec )
+        {}
+        
+        ~Array2DToString() = default;
+        
+        std::string header = "";
+        
+        std::string matrix_prefix = "{\n";
+        
+            std::string row_prefix = "\t";
+        
+            std::string row_suffix = "";
+        
+            std::string row_sep = ",\n";
+        
+        std::string matrix_suffix = "\n}";
+        
+        std::string footer = "";
+        
+        Array1DToString row_converter;
+        
+        std::string operator()( const * restrict const a, const size_t rows, const size_t cols ) const
         {
-            sout << a[0];
+            std::stringstream sout;
+            
+            sout << header;
+            
+            sout << matrix_prefix;
+            {
+                sout << row_prefix;
+                
+                if( rows > 0 )
+                {
+                    // write first row
+                    sout << row_converter( a, cols );
+                    
+                    // write remaining rows
+                    
+                    for( size_t i = 1; i < rows; ++i )
+                    {
+                        sout << row_suffix << row_sep << row_prefix;
+                        
+                        sout << row_converter( &a[cols*i], cols );
+                    }
+                }
+                
+                sout << row_suffix;
+            }
+            
+            sout << matrix_suffix;
+            
+            sout << footer;
+            
+            return sout.str();
         }
         
-        for( size_t i = 1; i < n; ++i )
-        {
-            sout << ", " << a[i];
-        }
-        sout << " }";
-        return sout.str();
-    }
+    } // Array2DToString
 
+    
+    
+    template<typename T>
+    std::string ToString( const * restrict const a, const size_t n, const int p = 16)
+    {
+        Array1DToString<T> s ();
+        
+        s.prec = p;
+        
+        return s(a, n);
+    }
+    
+    
+    template<typename T>
+    std::string ToString( const std::vector<T> & a, const int p = 16)
+    {
+        return ToString( a.data(), a.size(), p );
+    }
+    
 } // namespace Tools
