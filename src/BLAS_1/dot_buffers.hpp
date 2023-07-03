614a85d49a3dbaa8ef8f976893f0002652edb9c3
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2023-07-03 11:01:12 +0200

    Fixed recursive call.

--- src/BLAS_1/dot_buffers.hpp
+++ src/BLAS_1/dot_buffers.hpp
@@ -38,37 +38,30 @@ namespace Tools
         
         using T = decltype( R(1)*S(1) );
         
-        if( thread_count <= 1 )
+        if constexpr ( parQ == Parallel )
         {
-            return dot_buffers(x,y,n);
+            return ParallelDoReduce(
+                [=]( const Size_T i ) -> T
+                {
+                    return scalar_cast<T>( ox(x[i]) ) * scalar_cast<T>( oy(y[i]) );
+                },
+                []( const Size_T thread, const T & value, T & result ) -> T
+                {
+                    result += value;
+                },
+                n, static_cast<T>(0), thread_count
+            );
         }
         else
         {
-            if constexpr ( parQ == Parallel )
+            decltype( R(1) * S(1) ) sum = 0;
+            
+            for( Size_T i = 0; i < n; ++i )
             {
-                return ParallelDoReduce(
-                    [=]( const Size_T i ) -> T
-                    {
-                        return scalar_cast<T>( ox(x[i]) ) * scalar_cast<T>( oy(y[i]) );
-                    },
-                    []( const Size_T thread, const T & value, T & result ) -> T
-                    {
-                        result += value;
-                    },
-                    n, static_cast<T>(0), thread_count
-                );
+                sum += scalar_cast<T>( ox(x[i]) ) * scalar_cast<T>( oy(y[i]) );
             }
-            else
-            {
-                decltype( R(1) * S(1) ) sum = 0;
-                
-                for( Size_T i = 0; i < n; ++i )
-                {
-                    sum += scalar_cast<T>( ox(x[i]) ) * scalar_cast<T>( oy(y[i]) );
-                }
-                
-                return sum;
-            }
+            
+            return sum;
         }
     }
 
