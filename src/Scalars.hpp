ca7e040f87174b0934240eec988bd811c64734f3
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-09-07 13:02:35 -0400

    Code formatting.

--- src/Scalars.hpp
+++ src/Scalars.hpp
@@ -71,7 +71,7 @@ namespace Tools
     } // namespace Scalar
     
     
-
+    
     
     [[nodiscard]] std::string ToString( const Scalar::Flag flag )
     {
@@ -95,65 +95,59 @@ namespace Tools
             }
         }
     }
-
+    
     namespace Scalar
-    {    
+    {
         template<> constexpr bool ComplexQ<Real32>     = false;
         template<> constexpr bool ComplexQ<Real64>     = false;
-//        template<> constexpr bool ComplexQ<Real128>    = false;
+        //        template<> constexpr bool ComplexQ<Real128>    = false;
         template<> constexpr bool ComplexQ<Complex32>  = true;
         template<> constexpr bool ComplexQ<Complex64>  = true;
-//        template<> constexpr bool ComplexQ<Complex128> = true;
-
+        //        template<> constexpr bool ComplexQ<Complex128> = true;
+        
         
         template<> constexpr bool RealQ<Real32 >       = true;
         template<> constexpr bool RealQ<Real64 >       = true;
-//        template<> constexpr bool RealQ<Real128>       = true;
+        //        template<> constexpr bool RealQ<Real128>       = true;
         template<> constexpr bool RealQ<Complex32 >    = false;
         template<> constexpr bool RealQ<Complex64 >    = false;
-//        template<> constexpr bool RealQ<Complex128>    = false;
+        //        template<> constexpr bool RealQ<Complex128>    = false;
         
-        template<typename T> constexpr bool ScalarQ = false;
+        template<typename T> constexpr bool ScalarQ    = false;
         
         template<> constexpr bool ScalarQ<Real32 >     = true;
         template<> constexpr bool ScalarQ<Real64 >     = true;
-//        template<> constexpr bool ScalarQ<Real128>     = true;
+        //        template<> constexpr bool ScalarQ<Real128>     = true;
         template<> constexpr bool ScalarQ<Complex32 >  = true;
         template<> constexpr bool ScalarQ<Complex64 >  = true;
-//        template<> constexpr bool ScalarQ<Complex128>  = true;
+        //        template<> constexpr bool ScalarQ<Complex128>  = true;
         
         template<typename T> constexpr bool FloatQ = false;
         
         template<> constexpr bool FloatQ<Real32 >     = true;
         template<> constexpr bool FloatQ<Real64 >     = true;
-//        template<> constexpr bool FloatQ<Real128>     = true;
         template<> constexpr bool FloatQ<Complex32 >  = true;
         template<> constexpr bool FloatQ<Complex64 >  = true;
-//        template<> constexpr bool FloatQ<Complex128>  = true;
         
         template<typename T> constexpr int Prec       = 0;
         
         template<> constexpr int Prec<Real32 >     = sizeof(Real32) * 8;
         template<> constexpr int Prec<Real64 >     = sizeof(Real64) * 8;
-//        template<> constexpr int Prec<Real128>     = sizeof(Real128) * 8;
         template<> constexpr int Prec<Complex32 >  = sizeof(Real32) * 8;
         template<> constexpr int Prec<Complex64 >  = sizeof(Real64) * 8;
-//        template<> constexpr int Prec<Complex128>  = sizeof(Real64) * 8;
-  
+        
         
         template<typename T>
         using Real =
-            std::conditional_t<Prec<T> ==  32,  Real32,
-            std::conditional_t<Prec<T> ==  64,  Real64,
-//            std::conditional_t<Prec<T> == 128, Real128,
-            T>>;
-
+        std::conditional_t<Prec<T> ==  32,  Real32,
+        std::conditional_t<Prec<T> ==  64,  Real64,
+        T>>;
+        
         template<typename T>
         using Complex =
-            std::conditional_t<Prec<T> ==  32,  Complex32,
-            std::conditional_t<Prec<T> ==  64,  Complex64,
-//            std::conditional_t<Prec<T> == 128, Complex128,
-            T>>;
+        std::conditional_t<Prec<T> ==  32,  Complex32,
+        std::conditional_t<Prec<T> ==  64,  Complex64,
+        T>>;
         
         template<typename T> constexpr Scalar::Real<T> Zero  {0};
         template<typename T> constexpr Scalar::Real<T> One   {1};
@@ -254,23 +248,23 @@ namespace Tools
     template<typename T, typename S>
     force_inline constexpr
     typename std::enable_if_t<
-        Scalar::ScalarQ<T>
-        &&
-        Scalar::RealQ<S>,
-        typename Scalar::Real<T>
+    Scalar::ScalarQ<T>
+    &&
+    Scalar::RealQ<S>,
+    typename Scalar::Real<T>
     >
     scalar_cast( S x )
     {
         return static_cast<typename Scalar::Real<T>>(x);
     }
-
+    
     template<typename T, typename S>
     force_inline constexpr
     typename std::enable_if_t<
-        Scalar::ScalarQ<T>
-        &&
-        Scalar::ComplexQ<S>,
-        typename Scalar::Complex<T>
+    Scalar::ScalarQ<T>
+    &&
+    Scalar::ComplexQ<S>,
+    typename Scalar::Complex<T>
     >
     scalar_cast( cref<S> x )
     {
@@ -283,80 +277,12 @@ namespace Tools
     {
         return Scalar::One<R> / scalar_cast<R>(a);
     }
-
+    
     template<typename S, typename T, typename R = decltype( S(1)*T(1) )>
     force_inline constexpr R Frac( cref<S> a, cref<T> b )
     {
         return scalar_cast<R>(a) / scalar_cast<R>(b);
     }
-
-    
-//    // lo_prec_cast<S,T>(x) casts x to the lower precision of S and T, but preserves Real/Complex
-//    template<typename S, typename T, typename R>
-//    constexpr force_inline
-//    std::enable_if_t<
-//        Scalar::ScalarQ<S> && Scalar::ScalarQ<T>
-//        &&
-//        Scalar::RealQ<R>
-//        &&
-//        std::less_equal(Scalar::Prec<S>,Scalar::Prec<T>),
-//        typename Scalar::Real<S>
-//    >
-//    lo_prec_cast( R x )
-//    {
-//        return static_cast<typename Scalar::Real<S>>(x);
-//    }
-//
-//    // lo_prec_cast<S,T>(x) casts x to the lower precision of S and T
-//    template<typename S, typename T, typename R>
-//    constexpr force_inline
-//    std::enable_if_t<
-//        Scalar::ScalarQ<S> && Scalar::ScalarQ<T>
-//        &&
-//        Scalar::RealQ<R>
-//        &&
-//        std::greater(Scalar::Prec<S>, Scalar::Prec<T>),
-//        typename Scalar::Real<T>
-//    >
-//    lo_prec_cast( R x )
-//    {
-//        return static_cast<typename Scalar::Real<T>>(x);
-//    }
-//
-//
-//
-//    // hi_prec_cast<S,T>(x) casts x to the higher precision of S and T, but preserves Real/Complex
-//    template<typename S, typename T, typename R>
-//    constexpr force_inline
-//    std::enable_if_t<
-//        Scalar::ScalarQ<S> && Scalar::ScalarQ<T>
-//        &&
-//        Scalar::RealQ<R>
-//        &&
-//        std::greater_equal(Scalar::Prec<S>,Scalar::Prec<T>),
-//        typename Scalar::Real<S>
-//    >
-//    hi_prec_cast( R x )
-//    {
-//        return static_cast<typename Scalar::Real<S>>(x);
-//    }
-//
-//    // lo_prec_cast<S,T>(x) casts x to the lower precision of S and T
-//    template<typename S, typename T, typename R>
-//    constexpr force_inline
-//    std::enable_if_t<
-//        Scalar::ScalarQ<S> && Scalar::ScalarQ<T>
-//        &&
-//        Scalar::RealQ<R>
-//        &&
-//        std::less(Scalar::Prec<S>, Scalar::Prec<T>),
-//        typename Scalar::Real<T>
-//    >
-//    hi_prec_cast( R x )
-//    {
-//        return static_cast<typename Scalar::Real<T>>(x);
-//
-//    }
     
 //    template<typename Scal>
 //    inline constexpr bool NaNQ( cref<Scal> x )
@@ -394,31 +320,31 @@ namespace Tools
         }
     }
     
-//    template<typename Scal>
-//    inline constexpr bool NaNQ( cref<Scal> x )
-//    {
-//        // This is a work-around to detect NaNs under -ffast-math option.
-//        // Not sure whether this is portable.
-//        if constexpr ( Scalar::RealQ<Scal> )
-//        {
-////            return !(
-////                (std::numeric_limits<Scal>::lowest() <= x)
-////                &&
-////                ( x <=  std::numeric_limits<Scal>::max() )
-////            );
-//            
-//            // This works at least for Apple clang.
-//            return (x < std::numeric_limits<Scal>::lowest());
-//        }
-//        else if constexpr ( Scalar::ComplexQ<Scal> )
-//        {
-//            return NaNQ( Re(x) ) || NaNQ( Im(x) );
-//        }
-//        else
-//        {
-//            return true;
-//        }
-//    }
+    //    template<typename Scal>
+    //    inline constexpr bool NaNQ( cref<Scal> x )
+    //    {
+    //        // This is a work-around to detect NaNs under -ffast-math option.
+    //        // Not sure whether this is portable.
+    //        if constexpr ( Scalar::RealQ<Scal> )
+    //        {
+    ////            return !(
+    ////                (std::numeric_limits<Scal>::lowest() <= x)
+    ////                &&
+    ////                ( x <=  std::numeric_limits<Scal>::max() )
+    ////            );
+    //
+    //            // This works at least for Apple clang.
+    //            return (x < std::numeric_limits<Scal>::lowest());
+    //        }
+    //        else if constexpr ( Scalar::ComplexQ<Scal> )
+    //        {
+    //            return NaNQ( Re(x) ) || NaNQ( Im(x) );
+    //        }
+    //        else
+    //        {
+    //            return true;
+    //        }
+    //    }
     
 #define ASSERT_REAL(R) static_assert( Scalar::RealQ<R>, "Template parameter " #R " must be a real-valued type." );
     
@@ -427,44 +353,6 @@ namespace Tools
     
     namespace Scalar
     {
-//        template<Op op, typename T>
-//        T Operator( cref<T> x )
-//        {
-//            if constexpr ( op == Op::Id )
-//            {
-//                return x;
-//            }
-//            else if constexpr ( op == Op::Trans )
-//            {
-//                return x;
-//            }
-//            else if constexpr ( op == Op::Conj )
-//            {
-//                return Conj(x);
-//            }
-//            else if constexpr ( op == Op::ConjTrans )
-//            {
-//                return Conj(x);
-//            }
-//    //        else if constexpr ( op == Op::Re )
-//    //        {
-//    //            return Re(x);
-//    //        }
-//    //        else if constexpr ( op == Op::ReTrans )
-//    //        {
-//    //            return Re(x);
-//    //        }
-//    //        else if constexpr ( op == Op::Im )
-//    //        {
-//    //            return Im(x);
-//    //        }
-//    //        else if constexpr ( op == Op::ImTrans )
-//    //        {
-//    //            return Im(x);
-//    //        }
-//        };
-        
-        
         template<
             Tools::Op op, typename x_T,
             typename Return_T =
@@ -521,17 +409,20 @@ namespace Tools
             {
                 return static_cast<Return_T>(0);
             }
-        };
+        }
         
         template<
-            Scalar::Flag a_flag,Tools::Op op,
+            Scalar::Flag a_flag, Tools::Op op,
             typename a_T, typename x_T,
             typename Return_T = decltype( Op<a_flag>( a_T(0), Op<op>(x_T(0)) ) )
         >
         force_inline constexpr Return_T Op( cref<a_T> a, cref<x_T> x )
         {
-            return Op<a_flag,Return_T>( a, Op<op>(x) );
+            return Op<a_flag,a_T,x_T,Return_T>( a, Op<op>(x) );
         }
-    }
-}
+        
+    } // namespace Scalar
+    
+    
+} // namespace Tools
 
