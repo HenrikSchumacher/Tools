5ad1b513f449b9c0fdfdee30c426e73b574965be
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2025-01-31 13:28:24 -0500

    Added total_buffer.

---
+++ src/BLAS_1/total_buffer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace Tools
+{
+    
+    template< Size_T N = VarSize, Parallel_T parQ = Sequential, typename S >
+    [[nodiscard]] force_inline Scalar::Real<S> total_buffer(
+        cptr<S> x, const Size_T n = N, const Size_T thread_count = 1
+    )
+    {
+        using T = Scalar::Real<S>;
+        
+        {
+            #pragma float_control(precise, off)
+            
+            return DoReduce<N,parQ>(
+                [x]( const Size_T i )
+                {
+                    return x[i];
+                },
+                []( const T & value, T & result )
+                {
+                    result += value;
+                },
+                Scalar::Zero<T>, n, thread_count
+            );
+        }
+    }
+    
+} // namespace Tools
