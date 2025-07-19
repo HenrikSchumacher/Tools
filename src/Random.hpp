5b7de448d1b1d63a0c961e2cd8525c0f7912a5b9
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2025-07-19 17:59:01 +0200

    Added a simple routine that makes it easier to initialize STL conforming random engines.

---
+++ src/Random.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace Tools
+{
+    template<typename PRNG_T>
+    PRNG_T InitializedRandomEngine()
+    {
+        using RNG_T = std::random_device;
+        using RNG_result_type_T  = typename RNG_T::result_type;
+        
+        using PRNG_result_type_T = typename PRNG_T::result_type;
+        
+        static constexpr Size_T PRNG_T_state_size = 4;
+        
+        static constexpr Size_T seed_size  = (PRNG_T_state_size * sizeof(PRNG_result_type_T)) / sizeof(RNG_result_type_T);
+        
+        std::array<RNG_result_type_T,seed_size> seed;
+        
+        std::generate( seed.begin(), seed.end(), RNG_T() );
+        
+        std::seed_seq seed_sequence ( seed.begin(), seed.end() );
+        
+        return PRNG_T( seed_sequence );
+    }
+}
