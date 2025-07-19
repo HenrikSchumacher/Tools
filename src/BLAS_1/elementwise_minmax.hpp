c734adc1dd06e6094a4ce2631a020a047815f365
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2025-07-19 18:00:47 +0200

    Some minor bugfixes.

--- src/BLAS_1/elementwise_minmax.hpp
+++ src/BLAS_1/elementwise_minmax.hpp
@@ -4,10 +4,10 @@ namespace Tools
 {
     template<
         Size_T N = VarSize, Parallel_T parQ = Sequential,
-        typename x_T, typename y_T, typename up_T, typename Int = Size_T
+        typename x_T, typename y_T, typename z_T, typename Int = Size_T
     >
     TOOLS_FORCE_INLINE void elementwise_min(
-        cptr<x_T> x, cptr<y_T> y, mptr<up_T> up,
+        cptr<x_T> x, cptr<y_T> y, mptr<z_T> z,
         const Size_T n = N,
         const Size_T thread_count = 1
     )
@@ -16,26 +16,29 @@ namespace Tools
         
         check_sequential<parQ>( "elementwise_min", thread_count );
         
-        if constexpr ( (N > VarSize) && SameQ<x_T,y_T> && SameQ<x_T,up_T> && VectorizableQ<x_T> )
+        if constexpr ( (N > VarSize) && SameQ<y_T,z_T> && SameQ<x_T,z_T> && VectorizableQ<z_T> )
         {
-            using V_T = vec_T<N,x_T>;
+            using V_T = vec_T<N,z_T>;
             
             V_T x_vec;
             V_T y_vec;
             
-            copy_buffer<N>(x,&x_vec);
-            copy_buffer<N>(y,&y_vec);
+            copy_buffer<N>(x,reinterpret_cast<z_T*>(&x_vec));
+            copy_buffer<N>(y,reinterpret_cast<z_T*>(&y_vec));
             
             V_T z_vec = __builtin_elementwise_min(x_vec,y_vec);
             
-            copy_buffer<N>(&z_vec,up);
+            copy_buffer<N>(reinterpret_cast<z_T*>(&z_vec),z);
         }
         else
         {
             Do<N,parQ>(
                 [=]( const Size_T i )
                 {
-                    up[i] = Tools::Min(x[i], y[i]);
+                    z[i] = Tools::Min(
+                        static_cast<z_T>(x[i]),
+                        static_cast<z_T>(y[i])
+                    );
                 },
                 n, thread_count
             );
@@ -44,10 +47,10 @@ namespace Tools
     
     template<
         Size_T N = VarSize, Parallel_T parQ = Sequential,
-        typename x_T, typename lo_T
+        typename x_T, typename z_T
     >
     TOOLS_FORCE_INLINE void elementwise_min_update(
-        cptr<x_T> x, cptr<lo_T> lo,
+        cptr<x_T> x, mptr<z_T> lo,
         const Size_T n = N,
         const Size_T thread_count = 1
     )
@@ -56,19 +59,19 @@ namespace Tools
         
         check_sequential<parQ>( "elementwise_min", thread_count );
         
-        if constexpr ( (N > VarSize) && SameQ<x_T,lo_T> && VectorizableQ<x_T> )
+        if constexpr ( (N > VarSize) /*&& SameQ<x_T,lo_T>*/ && VectorizableQ<z_T> )
         {
-            using V_T = vec_T<N,x_T>;
+            using V_T = vec_T<N,z_T>;
             
             V_T x_vec;
             V_T y_vec;
             
-            copy_buffer<N>(x ,&x_vec);
-            copy_buffer<N>(lo,&y_vec);
+            copy_buffer<N>(x ,reinterpret_cast<z_T*>(&x_vec));
+            copy_buffer<N>(lo,reinterpret_cast<z_T*>(&y_vec));
             
             V_T z_vec = __builtin_elementwise_min(x_vec,y_vec);
             
-            copy_buffer<N>(&z_vec,lo);
+            copy_buffer<N>(reinterpret_cast<z_T*>(&z_vec),lo);
             
         }
         else
@@ -76,7 +79,7 @@ namespace Tools
             Do<N,parQ>(
                 [=]( const Size_T i )
                 {
-                    lo[i] = Tools::Min(x[i], lo[i]);
+                    lo[i] = Tools::Min(static_cast<z_T>(x[i]),lo[i]);
                 },
                 n, thread_count
             );
@@ -85,10 +88,10 @@ namespace Tools
     
     template<
         Size_T N = VarSize, Parallel_T parQ = Sequential,
-        typename x_T, typename y_T, typename up_T
+        typename x_T, typename y_T, typename z_T
     >
     TOOLS_FORCE_INLINE void elementwise_max(
-        cptr<x_T> x, cptr<y_T> y, mptr<up_T> up,
+        cptr<x_T> x, cptr<y_T> y, mptr<z_T> z,
         const Size_T n = N,
         const Size_T thread_count = 1
     )
@@ -97,26 +100,29 @@ namespace Tools
         
         check_sequential<parQ>( "elementwise_max", thread_count );
         
-        if constexpr ( (N > VarSize) && SameQ<x_T,y_T> && SameQ<x_T,up_T> && VectorizableQ<x_T> )
+        if constexpr ( (N > VarSize) && SameQ<y_T,z_T> && SameQ<x_T,z_T> && VectorizableQ<z_T> )
         {
-            using V_T = vec_T<N,x_T>;
+            using V_T = vec_T<N,z_T>;
             
             V_T x_vec;
             V_T y_vec;
             
-            copy_buffer<N>(x,&x_vec);
-            copy_buffer<N>(y,&y_vec);
+            copy_buffer<N>(x,reinterpret_cast<z_T*>(&x_vec));
+            copy_buffer<N>(y,reinterpret_cast<z_T*>(&y_vec));
             
             V_T z_vec = __builtin_elementwise_max(x_vec,y_vec);
             
-            copy_buffer<N>(&z_vec,up);
+            copy_buffer<N>(reinterpret_cast<z_T*>(&z_vec),z);
         }
         else
         {
             Do<N,parQ>(
                 [=]( const Size_T i )
                 {
-                    up[i] = Tools::Max(x[i], y[i]);
+                    z[i] = Tools::Max(
+                        static_cast<z_T>(x[i]),
+                        static_cast<z_T>(y[i])
+                    );
                 },
                 n, thread_count
             );
@@ -125,10 +131,10 @@ namespace Tools
     
     template<
         Size_T N = VarSize, Parallel_T parQ = Sequential,
-        typename x_T, typename hi_T
+        typename x_T, typename z_T
     >
     TOOLS_FORCE_INLINE void elementwise_max_update(
-        cptr<x_T> x, cptr<hi_T> hi,
+        cptr<x_T> x, mptr<z_T> z,
         const Size_T n = N,
         const Size_T thread_count = 1
     )
@@ -137,26 +143,26 @@ namespace Tools
         
         check_sequential<parQ>( "elementwise_max", thread_count );
         
-        if constexpr ( (N > VarSize) && SameQ<x_T,hi_T> && VectorizableQ<x_T> )
+        if constexpr ( (N > VarSize) && SameQ<x_T,z_T> && VectorizableQ<z_T> )
         {
-            using V_T = vec_T<N,x_T>;
+            using V_T = vec_T<N,z_T>;
             
             V_T x_vec;
             V_T y_vec;
             
-            copy_buffer<N>(x,&x_vec);
-            copy_buffer<N>(hi,&y_vec);
+            copy_buffer<N>(x,reinterpret_cast<z_T*>(&x_vec));
+            copy_buffer<N>(z,reinterpret_cast<z_T*>(&y_vec));
             
             V_T z_vec = __builtin_elementwise_max(x_vec,y_vec);
             
-            copy_buffer<N>(&z_vec,hi);
+            copy_buffer<N>(reinterpret_cast<z_T*>(&z_vec),z);
         }
         else
         {
             Do<N,parQ>(
                 [=]( const Size_T i )
                 {
-                    hi[i] = Tools::Max(x[i], hi[i]);
+                    z[i] = Tools::Max( static_cast<z_T>(x[i]),z[i] );
                 },
                 n, thread_count
             );
@@ -165,10 +171,10 @@ namespace Tools
     
     template<
         Size_T N = VarSize, Parallel_T parQ = Sequential,
-        typename x_T, typename y_T, typename lo_T, typename hi_T
+        typename x_T, typename y_T, typename z_T
     >
     TOOLS_FORCE_INLINE void elementwise_minmax(
-        cptr<x_T> x, cptr<y_T> y, mptr<lo_T> lo, mptr<hi_T> up,
+        cptr<x_T> x, cptr<y_T> y, mptr<z_T> lo, mptr<z_T> hi,
         const Size_T n = N,
         const Size_T thread_count = 1
     )
@@ -178,28 +184,31 @@ namespace Tools
         check_sequential<parQ>( "elementwise_minmax", thread_count );
 
         
-        if constexpr ( (N > VarSize) && SameQ<x_T,y_T> && SameQ<x_T,lo_T> && SameQ<x_T,hi_T> && VectorizableQ<x_T> )
+        if constexpr ( (N > VarSize) && VectorizableQ<z_T> )
         {
-            using V_T = vec_T<N,x_T>;
+            using V_T = vec_T<N,z_T>;
             
             V_T x_vec;
             V_T y_vec;
             
-            copy_buffer<N>(x,&x_vec);
-            copy_buffer<N>(y,&y_vec);
+            copy_buffer<N>(x,reinterpret_cast<z_T*>(&x_vec));
+            copy_buffer<N>(y,reinterpret_cast<z_T*>(&y_vec));
             
             V_T lo_vec = __builtin_elementwise_min(x_vec,y_vec);
-            V_T up_vec = __builtin_elementwise_max(x_vec,y_vec);
+            V_T hi_vec = __builtin_elementwise_max(x_vec,y_vec);
             
-            copy_buffer<N>(&lo_vec,lo);
-            copy_buffer<N>(&up_vec,up);
+            copy_buffer<N>(reinterpret_cast<z_T*>(&lo_vec),lo);
+            copy_buffer<N>(reinterpret_cast<z_T*>(&hi_vec),hi);
         }
         else
         {
             Do<N,parQ>(
                 [=]( const Size_T i )
                 {
-                    std::tie( lo[i], up[i] ) = Tools::MinMax(x[i], y[i]);
+                    std::tie(lo[i],hi[i]) = Tools::MinMax(
+                        static_cast<z_T>(x[i]),
+                        static_cast<z_T>(y[i])
+                    );
                 },
                 n, thread_count
             );
@@ -208,10 +217,10 @@ namespace Tools
     
     template<
         Size_T N = VarSize, Parallel_T parQ = Sequential,
-        typename x_T, typename lo_T, typename up_T
+        typename x_T, typename z_T
     >
     TOOLS_FORCE_INLINE void elementwise_minmax_update(
-        cptr<x_T> x, mptr<lo_T> lo, mptr<up_T> up,
+        cptr<x_T> x, mptr<z_T> lo, mptr<z_T> hi,
         const Size_T n = N,
         const Size_T thread_count = 1
     )
@@ -220,23 +229,21 @@ namespace Tools
         
         check_sequential<parQ>( "elementwise_minmax", thread_count );
         
-        if constexpr ( (N > VarSize) && SameQ<x_T,lo_T> && SameQ<x_T,up_T> && VectorizableQ<x_T> )
+        if constexpr ( (N > VarSize) /*&& SameQ<x_T,z_T>*/ && VectorizableQ<z_T> )
         {
-            using V_T = vec_T<N,x_T>;
+            using V_T = vec_T<N,z_T>;
             
             V_T x_vec;
             V_T lo_vec;
-            V_T up_vec;
+            V_T hi_vec;
             
-            copy_buffer<N>(x,&x_vec);
-            copy_buffer<N>(&lo_vec,lo);
-            copy_buffer<N>(&up_vec,up);
+            copy_buffer<N>(x,reinterpret_cast<z_T*>(&x_vec));
             
             lo_vec = __builtin_elementwise_min(x_vec,lo_vec);
-            up_vec = __builtin_elementwise_max(x_vec,up_vec);
+            hi_vec = __builtin_elementwise_max(x_vec,hi_vec);
             
-            copy_buffer<N>(&lo_vec,lo);
-            copy_buffer<N>(&up_vec,up);
+            copy_buffer<N>(reinterpret_cast<z_T*>(&lo_vec),lo);
+            copy_buffer<N>(reinterpret_cast<z_T*>(&hi_vec),hi);
         }
         else
         {
@@ -245,8 +252,8 @@ namespace Tools
             Do<N,parQ>(
                 [=]( const Size_T i )
                 {
-                    lo[i] = Tools::Min( x[i], lo[i] );
-                    up[i] = Tools::Max( x[i], up[i] );
+                    lo[i] = Tools::Min(static_cast<z_T>(x[i]),lo[i]);
+                    hi[i] = Tools::Max(static_cast<z_T>(x[i]),hi[i]);
                 },
                 n, thread_count
             );
