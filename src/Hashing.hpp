eaa5fda2910fc73f9007f3e6c07d6980a1072916
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2025-08-24 01:23:21 +0200

    Hasing for arrays.

--- src/Hashing.hpp
+++ src/Hashing.hpp
@@ -5,22 +5,37 @@ namespace Tools {
     // https://stackoverflow.com/a/9729747/8248900
 
     template <class T>
-    inline void hash_combine(std::size_t & seed, const T & v)
+    inline void hash_combine(std::size_t & seed, const T & x)
     {
         using namespace std;
         hash<T> hasher;
-        seed ^= hasher(v) + std::size_t(0x9e3779b9) + (seed << 6) + (seed >> 2);
+        seed ^= hasher(x) + std::size_t(0x9e3779b9) + (seed << 6) + (seed >> 2);
     }
 
     template<typename S, typename T>
     struct pair_hash
     {
-        inline std::size_t operator()(const std::pair<S,T> & v) const
+        inline std::size_t operator()(const std::pair<S,T> & p) const
         {
-             std::size_t seed = 0;
-             hash_combine(seed, v.first);
-             hash_combine(seed, v.second);
-             return seed;
+            std::size_t seed = 0;
+            hash_combine(seed, p.first);
+            hash_combine(seed, p.second);
+            return seed;
+        }
+    };
+    
+    
+    template<typename T, std::size_t n>
+    struct array_hash
+    {
+        inline std::size_t operator()(const std::array<T,n> & a) const
+        {
+            std::size_t seed = 0;
+            for( std::size_t i = 0; i < n; ++i )
+            {
+                hash_combine(seed, a[i]);
+            }
+            return seed;
         }
     };
 
