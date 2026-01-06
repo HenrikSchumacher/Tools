82886e74691011fe063483a77c58c2ae84fbadfd
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2026-01-06 21:23:59 +0100

    Maintenance.

--- src/Debugging.hpp
+++ src/Debugging.hpp
@@ -9,108 +9,12 @@ namespace Tools
         static_assert(IntQ<Int_1>,"");
         static_assert(IntQ<Int_0>,"");
         
-//#ifdef TOOLS_DEBUG
         if( !std::in_range<Int_1>( n ) )
         {
-            eprint(std::string("int_cast<") + TypeName<Int_1> + "," + TypeName<Int_0> + "> reports integer overflow for n = " + ToString(n) + " of type " + TypeName<Int_0> + ".");
+            eprint(std::string("int_cast<") + TypeName<Int_1> + "," + TypeName<Int_0> + "> reports integer overflow for n = " + ToString(n) + ".");
         }
-//#endif
         
         return static_cast<Int_1>(n);
-        
-        
-//        static_assert(IntQ<Int_0>,"");
-//        static_assert(IntQ<Int_1>,"");
-//        
-//        using Lim_0 = std::numeric_limits<Int_0>;
-//        using Lim_1 = std::numeric_limits<Int_1>;
-//        
-//        
-//        constexpr bool positive_overflow_possible = std::cmp_lower(Lim_1::max(),Lim_0::max());
-//        constexpr bool negative_overflow_possible =
-//        Lim_0::is_signed
-//        ||
-//        std::cmp_greater( Lim_1::lowest(), Lim_0::lowest() );
-//        
-//        
-//        // unsigned <-- unsigned
-//        if constexpr ( (! Lim_1::is_signed) && (! Lim_0::is_signed) )
-//        {
-//            if constexpr (positive_overflow_possible )
-//            {
-//#ifdef TOOLS_DEBUG
-//                if ( std::cmp_greater(n,Lim_1::max()) )
-//                {
-//                    eprint(std::string("int_cast<") + TypeName<Int_1> + "> reports integer overflow n > " + ToString(Lim_1::max()) + "  for n = " + ToString(n) + " of type " + TypeName<Int_0> + ".");
-//                }
-//#endif
-//            }
-//        }
-//        
-//        // unsigned <-- signed
-//        if constexpr( (! Lim_1::is_signed) && Lim_0::is_signed )
-//        {
-//            if constexpr (positive_overflow_possible )
-//            {
-//#ifdef TOOLS_DEBUG
-//                if( std::cmp_greater(n,Lim_1::max()) )
-//                {
-//                    eprint(std::string("int_cast<") + TypeName<Int_1> + "> reports integer overflow n > " + ToString(Lim_1::max()) + "  for n = " + ToString(n) + " of type " + TypeName<Int_0> + ".");
-//                }
-//#endif
-//            }
-//            
-//            if constexpr (negative_overflow_possible )
-//            {
-//#ifdef TOOLS_DEBUG
-//                if ( std::cmp_lower(n,0) )
-//                {
-//                    eprint(std::string("int_cast<") + TypeName<Int_1> + "> reports integer underflow n < 0 for n = " + ToString(n) + " of type " + TypeName<Int_0> + ".");
-//                }
-//#endif
-//            }
-//            
-//        }
-//        
-//        // signed <-- unsigned
-//        if constexpr ( Lim_1::is_signed && !Lim_0::is_signed )
-//        {
-//            if constexpr ( positive_overflow_possible )
-//            {
-//#ifdef TOOLS_DEBUG
-//                if( std::cmp_greater(n,Lim_1::max()) )
-//                {
-//                    eprint(std::string("int_cast<") + TypeName<Int_1> + "> reports integer overflow n > " + ToString(Lim_1::max()) + "  for n = " + ToString(n) + " of type " + TypeName<Int_0> + ".");
-//                }
-//#endif
-//            }
-//        }
-//        
-//        // signed <-- signed
-//        if constexpr ( Lim_1::is_signed && Lim_0::is_signed )
-//        {
-//            if constexpr( positive_overflow_possible )
-//            {
-//#ifdef TOOLS_DEBUG
-//                if( std::cmp_greater(n,Lim_1::max()) )
-//                {
-//                    eprint(std::string("int_cast<") + TypeName<Int_1> + "> reports integer overflow n > " + ToString(Lim_1::max()) + "  for n = " + ToString(n) + " of type " + TypeName<Int_0> + ".");
-//                }
-//#endif
-//            }
-//            
-//            if constexpr (negative_overflow_possible )
-//            {
-//#ifdef TOOLS_DEBUG
-//                if ( std::cmp_lower(n,Lim_1::lowest()) )
-//                {
-//                    eprint(std::string("int_cast<") + TypeName<Int_1> + "> reports integer underflow n < " + ToString(Lim_1::lowest()) + "  for n = " + ToString(n) + " of type " + TypeName<Int_0> + ".");
-//                }
-//#endif
-//            }
-//        }
-//        
-//        return static_cast<Int_1>(n);
     }
     
     
